6cf62d400e994b07ce02aad0b49e340632735087
Wealthometer <wealthhajoh87@gmail.com>
2025-07-01 23:16:40 +0100

    Update database.ejs

--- server/models/database.js
+++ server/models/database.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser : true, useUnifiedTopology  : true })
 
 const db = mongoose.connection;
-
+db.on('error' , console.error(console , "Connection Error"))
