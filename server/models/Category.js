cffc669724311f751fbfc990fa7424bf72d368d1
Wealthometer <wealthhajoh87@gmail.com>
2025-07-01 23:58:41 +0100

    Update

--- server/models/Category.js
+++ server/models/Category.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema({
     type : String,
     required : 'This field is required.'
   },
-  
+
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model('Category', categorySchema); 
