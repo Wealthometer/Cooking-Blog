d10d19517e424fad35c704421dd1996dbba1edfb
Wealthometer <wealthhajoh87@gmail.com>
2025-07-01 23:46:25 +0100

    Update

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -3,3 +3,37 @@
 exports.homepage = async(req, res) => {
     res.render('index', { title : 'Cooking-Blog - Home' });
 }
+
+
+async function insertDymmyCategoryData(){
+  try {
+    await Category.insertMany([
+      {
+        "name": "Thai",
+        "image": "thai-food.jpg"
+      },
+      {
+        "name": "American",
+        "image": "american-food.jpg"
+      }, 
+      {
+        "name": "Chinese",
+        "image": "chinese-food.jpg"
+      },
+      {
+        "name": "Mexican",
+        "image": "mexican-food.jpg"
+      }, 
+      {
+        "name": "Indian",
+        "image": "indian-food.jpg"
+      },
+      {
+        "name": "Spanish",
+        "image": "spanish-food.jpg"
+      }
+    ]);
+  } catch (error) {
+    console.log('err', + error)
+  }
+}
