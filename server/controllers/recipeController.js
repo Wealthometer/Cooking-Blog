972d2c662b9f53000d15ab404e9f01f328b28e42
Wealthometer <wealthhajoh87@gmail.com>
2025-07-03 14:14:17 +0100

    Update an Upload

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -9,9 +9,16 @@ exports.homepage = async(req, res) => {
     const limitNumber = 5;
     const categories = await Category.find({}).limit(limitNumber);
 
-    res.render('index', { title : 'Cooking-Blog - Home', categories });
+    const latest = await Recipe.find({}).sort({_id  : 1}).limit(limitNumber);
+        const thai = await Recipe.find({ 'category': 'Thai' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+    const food = { latest, thai, american, chinese };
+
+    res.render('index', { title : 'Cooking-Blog - Home', categories, food });
   } catch (error) {
-    res.status(500).send({message: error.message || "Error Occured"})
+    res.status(500).send({message: error.message || "Error Occured"}).limit(limitNumber);
   }
 
 }
