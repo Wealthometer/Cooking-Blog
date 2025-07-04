730175b9b65fc53e74d610805312495abc0c7cc5
Wealthometer <wealthhajoh87@gmail.com>
2025-07-04 13:44:57 +0100

    Update recipeRoutes.js

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -7,5 +7,6 @@ router.get('/' , recipeController.homepage);
 router.get('/categories' , recipeController.exploreCategories);
 router.get('/categories/:id' , recipeController.exploreCategoriesById);
 router.get('/recipe/:id' , recipeController.exploreRecipe);
+router.get('/search' , recipeController.searchRecipe);
 
 module.exports = router;
